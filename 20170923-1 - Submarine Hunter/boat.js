a1f0672d34c5d1a4de5abb7534bbb1d9290d3aaa
Wesfar <jose.goncalves.pt@gmail.com>
2017-10-09 00:53:20 +0100

    Advanced Touch

---
+++ 20170923-1 - Submarine Hunter/boat.js
@@ -0,0 +1,76 @@
+function Boat() {
+
+  this.width = 50;
+  this.height = 10;  
+  
+  this.color = "rgba(255,255,255,0.40)";
+  
+  this.pos = new vector(
+    utils.randomRange(0,1) * width * 4 / 5 + width * 1 / 10,
+    height * 1 / 5 - this.height + 3
+  );
+
+  this.velocity = new vector(
+    0,
+    0
+  );
+
+  this.move = function () {
+    if (this.isAcceleratingLeft) {  this.velocity._x += -0.1 }
+    if (this.isAcceleratingRight) { this.velocity._x += 0.1 }
+    this.pos._x += this.velocity._x;
+  };
+
+  this.edge = function () {
+    if (this.pos._x > width) { this.pos._x = -this.width};
+    if (this.pos._x < -this.width ) { this.pos._x = width };
+  };
+
+  this.draw = function () {
+    ctxSea.save();
+
+    ctxSea.translate(this.pos._x, this.pos._y);
+
+    ctxSea.fillStyle = this.color;
+    ctxSea.strokeStyle = "black";
+    ctxSea.lineWidth = 1;
+
+    ctxSea.beginPath(); 
+    utils.roundRect(ctxSea,0, 0, this.width, this.height,{ tl: 0, tr: 0, br: 4, bl: 4 });
+    ctxSea.closePath(); 
+    ctxSea.fill();
+    ctxSea.stroke();
+
+    ctxSea.beginPath(); 
+    ctxSea.rect(this.width * 3 / 10, -this.height * 2 / 3, this.width * 4 / 10, this.height * 2 / 3);
+    ctxSea.rect(this.width * 4 / 10, -this.height, this.width * 2 / 10, this.height*1/3);
+    ctxSea.closePath(); 
+    ctxSea.fill();
+    ctxSea.stroke();
+
+    
+    ctxSea.strokeStyle = "black";
+    ctxSea.fillStyle = "darkblue";
+    ctxSea.lineWidth = 1;
+    
+    ctxSea.beginPath(); 
+    ctxSea.ellipse(this.width * 1 / 4, this.height * 1 / 2, this.height * 1 / 5, this.height * 1 / 5,0,0,Math.PI*2);
+    ctxSea.closePath();
+    ctxSea.fill();
+    ctxSea.stroke();
+    ctxSea.beginPath(); 
+    ctxSea.ellipse(this.width * 2 / 4, this.height * 1 / 2, this.height * 1/5, this.height * 1/5,0,0,Math.PI*2);
+    ctxSea.closePath();
+    ctxSea.fill();
+    ctxSea.stroke();
+    ctxSea.beginPath(); 
+    ctxSea.ellipse(this.width * 3 / 4, this.height * 1 / 2, this.height * 1/5, this.height * 1/5,0,0,Math.PI*2);
+    ctxSea.closePath();
+    ctxSea.fill();
+    ctxSea.stroke();
+
+    
+    ctxSea.restore();
+  };
+
+}
---
+++ 20170923-1 - Submarine Hunter/script.js
@@ -0,0 +1,210 @@
+"use strict"
+
+let canvasSea = document.getElementById("canvasSea"),
+ctxSea = canvasSea.getContext("2d");
+
+let canvasExplosion = document.getElementById("canvasExplosion"),
+ctxExplosion = canvasExplosion.getContext("2d");
+
+let width = canvasSea.width = canvasExplosion.width = window.innerWidth;
+let height = canvasSea.height = canvasExplosion.height = window.innerHeight;
+
+let mines = [];
+let mineReloadTime = 2000;
+let mineReloadTimer = 0;
+let mineReloadStart = new Date().getTime();
+let maxMines = 3;
+
+let shots = 0;
+let hits = 0;
+
+let explosions = [];
+
+let timeStart = new Date();
+let timeElapsed;
+
+let touchStartDate = 0;
+let touchEndDate = 0;
+let touchDuration = 0;
+let touchTimer = 0;
+let touchX = 0;
+
+
+let reloader = new Reloader(width-20, 60);
+
+let submarines = [];
+for (let i = 0; i < 15; i++) {
+  submarines.push(new Submarine);
+};
+
+let boat = new Boat();
+
+window.onload = startup();
+
+function startup (){
+//  window.statusbar= false;
+//  window.toolbar = false;
+  console.log("Start");
+  loop();
+};
+
+
+// Game loop
+function loop() {
+
+  ctxExplosion.clearRect(0, 0, width, height);
+
+  ctxSea.fillStyle = "lightblue";
+  ctxSea.fillRect(0, 0, width, height);
+
+  ctxSea.fillStyle = "blue";
+  ctxSea.fillRect(0, height * 1 / 5, width, height);
+
+  
+  timeElapsed = Math.floor((new Date() - timeStart) / 100) / 10;
+
+  ctxSea.lineWidth = 10;
+  ctxSea.fillStyle = "black";
+  ctxSea.fillText("Shots: " + shots, 10, 20)
+  ctxSea.fillText("Hits: " + hits, 10, 30)
+  ctxSea.fillText("Mines: " + mines.length, 10, 40)
+  ctxSea.fillText("Submarines: " + submarines.length, 10, 50)
+  ctxSea.fillText("Time: " + timeElapsed, 10, 60)
+  
+
+  for (let i = 0; i < submarines.length; i++) {
+    submarines[i].move();
+    submarines[i].edge();
+    submarines[i].draw();
+  };
+
+  for (let i = explosions.length - 1; i >= 0; i--) {
+    explosions[i].draw();
+    if (explosions[i].exploding = false) { explosions.splice(i, 1) };
+  };
+
+
+  for (let i = mines.length - 1; i >= 0; i--) {
+    mines[i].move();
+    mines[i].draw();
+    for (let j = submarines.length - 1; j >= 0; j--) {
+      if (mines[i].hits(submarines[j])) {
+        submarines.splice(j, 1);
+        mines[i].hit = true;
+        hits++;
+        explosions.push(new Explosion(mines[i].pos._x, mines[i].pos._y));
+      };
+    };
+    if (mines[i].pos._y + mines[i].radius > height || mines[i].hit == true) {
+      mines.splice(i, 1);
+    };
+  };
+
+  boat.move();
+  boat.edge();
+  boat.draw();
+
+  mineTimer(new Date().getTime());
+  reloader.draw();
+
+  
+  touchTimer = new Date().getTime();
+
+  if (touchEndDate < touchStartDate && touchTimer - touchStartDate > 100) {         // touch em curso e maior que apenas bomba
+    if (touchX > width / 2) { boat.velocity._x += +0.1 }
+    if (touchX < width / 2) { boat.velocity._x += -0.1 }
+  };
+
+  if (touchEndDate > touchStartDate) {         // não há touch em curso
+    boat.velocity._x += +0.0
+  };
+
+
+  requestAnimationFrame(loop); //chamar a propria funcao "desenhar" sempre que o ecra esteja pronto para processar nova frame
+  
+  
+};
+
+
+document.body.addEventListener("keydown", keyDown,false);
+document.body.addEventListener("keyup", keyUp,false);
+document.body.addEventListener("touchend", touchEnd,false);
+document.body.addEventListener("touchstart", touchStart,false);
+//document.body.addEventListener("touchmove", touchMove,false);
+
+function touchStart(e) {
+  e.preventDefault();
+  touchStartDate = new Date().getTime();
+  touchX = e.changedTouches[0].clientX;
+  console.log("touchStartDate: "+touchStartDate);
+};
+
+function touchEnd(e) {
+  e.preventDefault();
+  touchEndDate = new Date().getTime(); 
+  touchDuration = touchEndDate - touchStartDate; 
+  console.log("touchEndDate: " + touchEndDate);
+  console.log("touchDuration: " + touchDuration);
+  
+  if (touchDuration < 100) {
+    mineTimerCheck(new Date());
+  };
+
+};
+
+/*
+
+function touchMove(e) {
+  e.preventDefault();  
+
+  varTouchX = e.changedTouches[0].clientX - width / 2;
+
+  if (varTouchX > 0) { boat.isMovingRight = true };
+  if (varTouchX < 0) { boat.isMovingLeft = true };
+
+  console.log("Move " + newTouchX + " " + "Var " + varTouchX);
+//  mineTimerCheck(new Date());
+};
+*/
+
+function keyUp(e) {
+  switch (event.keyCode) {
+    case 37: //left
+      boat.velocity._x += -0.0;
+      break;
+    case 39: //right
+      boat.velocity._x += +0.0;
+      break;
+    default:
+      break;
+  };
+};  
+
+function keyDown(e) {
+  switch (event.keyCode) {
+    case 37: //left
+      boat.velocity._x += -0.1;
+      break;
+    case 39: //right
+      boat.velocity._x += +0.1;
+    break;
+    case 40: //down
+      mineTimerCheck(new Date().getTime());
+        break;
+    default:
+        break;
+  }
+};
+
+function mineTimerCheck(x) {
+  mineReloadTimer = x - mineReloadStart;
+  if (mineReloadTimer > mineReloadTime) {
+    mines.push(new Mine(boat.pos._x + boat.width / 2));
+    mineReloadStart = new Date().getTime();
+    shots++;
+  }
+}
+
+function mineTimer(x) {
+  mineReloadTimer = x - mineReloadStart;
+}
