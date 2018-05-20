8641bd121e143399d34ed17ee6cb61f99bfb24c1
Wesfar <jose.goncalves.pt@gmail.com>
2018-05-20 14:57:59 +0100

    AS PWA 22

--- 20180415-1 - Air Strike/canvas.js
+++ 20180415-1 - Air Strike/canvas.js
@@ -25,3 +25,17 @@ class Canvas {
   }
 };
 
+
+document.body.addEventListener('touchmove', function(event) {
+  console.log(event.source);
+  //if (event.source == document.body)
+    event.preventDefault();
+}, false);
+
+window.onresize = function() {
+  $(document.body).width(window.innerWidth).height(window.innerHeight);
+}
+
+$(function() {
+  window.onresize();
+});
