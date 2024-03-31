5f751aa4baa1902968c2b21421ed26d449e2aed3
Wesfar <jose.goncalves.pt@gmail.com>
2024-03-31 00:07:33 +0000

    Fixed ISS Locator

--- 20161202-1 - Google Maps ISS Coordinates/script.js
+++ 20161202-1 - Google Maps ISS Coordinates/script.js
@@ -5,7 +5,7 @@ https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/JSON#Getting_s
 var ISS_Location;
 var ISS_URL = 'https://api.wheretheiss.at/v1/satellites/25544';
 
-var PlotInterval = 1*10*1000;
+var PlotInterval = 2*1000;
 var LabelInterval = PlotInterval;
 
 function request_ISS_Position() {
@@ -22,24 +22,21 @@ function request_ISS_Position() {
 request_ISS_Position();
 
 var map;
-var infoWindow;
 
 function init_Map() {
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 2,
     center: { lat: 0.0, lng: 0.0 },
-    mapTypeId: 'terrain'   
+    mapTypeId: 'terrain',
+    mapId: "DEMO_MAP_ID" // Map ID is required for advanced markers.   
   });
 }
-
-//initMap();
 
 function plot_Marker() {
   var LatLng_ISS = new google.maps.LatLng(ISS_Location.latitude, ISS_Location.longitude);
   var marker = new google.maps.Marker({
     map: map,   // Indicação do Map ao qual se quer adicionar os Markers (neste caso é o Object "map" que criei acima)
-    animation: google.maps.Animation.DROP,
-    title: "ISS@" + new Date().toLocaleTimeString(),
+    //animation: google.maps.Animation.DROP,
     position: LatLng_ISS,
     icon: 'iss-verysmall.png'
   }
@@ -47,46 +44,9 @@ function plot_Marker() {
 }
 
 
-function plot_Label_1() {
-  var LatLng_ISS = new google.maps.LatLng(ISS_Location.latitude, ISS_Location.longitude);
-  var infoWindow_1 = new google.maps.InfoWindow({
-    map: map,  // Indicação do Map ao qual se quer adicionar os Markers (neste caso é o Object "map" que criei acima)
-    content: "ISS@" + new Date().toLocaleTimeString(),
-    position: LatLng_ISS
-  }
-  );
-}
-  
-function plot_Label() {
-  var LatLng_ISS = new google.maps.LatLng(ISS_Location.latitude, ISS_Location.longitude);
-  infoWindow = new google.maps.InfoWindow({
-  map: map,  // Indicação do Map ao qual se quer adicionar os Markers (neste caso é o Object "map" que criei acima)
-  content: "ISS@" + new Date().toLocaleTimeString(),
-  position: LatLng_ISS
-  }
-  );
-
-  setInterval(draw_Label, LabelInterval);
-}
-
-function update_Label() {
-  var LatLng_ISS = new google.maps.LatLng(ISS_Location.latitude, ISS_Location.longitude);
-  infoWindow.setPosition(LatLng_ISS);
-  infoWindow.setContent("ISS@" + new Date().toLocaleTimeString());
-  }
-
 function draw_Marker() {
   request_ISS_Position();
   plot_Marker();
 }
 
 setInterval(draw_Marker, PlotInterval);
-setTimeout(plot_Marker, 1000)
-
-function draw_Label() {
-  request_ISS_Position();
-  update_Label();
-}
-
-setTimeout(plot_Label_1, 1000);
-setTimeout(plot_Label, 1000)
