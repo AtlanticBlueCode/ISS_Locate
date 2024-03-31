f157db97d275f71bc989610d2c49156c9323cb89
Wesfar <jose.goncalves.pt@gmail.com>
2024-03-31 03:09:54 +0100

    ISS Locator improved

--- 20161202-1 - Google Maps ISS Coordinates/script.js
+++ 20161202-1 - Google Maps ISS Coordinates/script.js
@@ -1,8 +1,9 @@
 // Improved ISS Tracker Code
 
 // Constants
-const ISS_URL = 'https://api.wheretheiss.at/v1/satellites/25544';
+const ISS_URL = 'http://api.open-notify.org/iss-now.json?key=ZRlcJrbb0wvyApa0QdayeQuweYmeYaSGrafFRePK';
 const PLOT_INTERVAL = 2000; // 1 seconds
+let firstPlot = true;
 
 var map;
 
@@ -20,32 +21,55 @@ function initMap() {
   fetchISSPosition(map);
 }
 
-// Fetch ISS position from the API
-async function fetchISSPosition(map) {
-  try {
-    const response = await fetch(ISS_URL);
-    if (!response.ok) {
-      throw new Error('Failed to fetch ISS data');
-    }
-    const ISS_Location = await response.json();
+
+async function fetchISSPosition(map){
+  fetch(ISS_URL)
+  .then(response => response.json())
+  .then(data => {
+
+    // Extract latitude and longitude to ISS_Location variable
+    const ISS_Location = {
+      latitude: parseFloat(data.iss_position.latitude), 
+      longitude: parseFloat(data.iss_position.longitude)
+    };
+
+    // Print the results
+    console.log(`ISS current location: Latitude ${ISS_Location.latitude}, Longitude ${ISS_Location.longitude}`);
+
+    // Plot Marker
     plotMarker(map, ISS_Location);
-  } catch (error) {
-    console.error('Error fetching ISS data:', error.message);
-  }
+
+  })
+  .catch(error => {
+    console.error("Error fetching ISS location:", error);
+  });
+
 }
+
 
 // Plot ISS marker on the map
 function plotMarker(map, ISS_Location) {
+  // Create a google maps LatLng Object from the ISS_Location Object
   const LatLng_ISS = new google.maps.LatLng(
     ISS_Location.latitude,
     ISS_Location.longitude
   );
 
+  // Center the map on the first time the marker is drawn
+  if (firstPlot){
+    map.setCenter(LatLng_ISS)
+  } 
+
+  // Do not center again
+  firstPlot = false
+
+  // Plot new Marker
   new google.maps.Marker({
     map,
     position: LatLng_ISS,
     icon: 'iss-verysmall.png',
   });
+
 }
 
 // Periodically update ISS position
